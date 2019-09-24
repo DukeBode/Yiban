89c0002b3755c50ca5e8b0758965ea58b6974373
DukeBode <Duke123@aliyun.com>
2019-09-24 23:14:56 +0800

    2.0

--- install.bat
+++ install.bat
@@ -11,7 +11,7 @@ echo 正在复制文件
 copy program\Template.docx .\
 
 copy program\Ecode_v1.0.py eqrcode.py
-copy program\Edata_v1.5.py edata.py
+copy program\Edata_v2.0.py edata.py
 pause
 
 echo 创建 code 文件夹
