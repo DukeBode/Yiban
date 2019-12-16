2f620c0b223a38b35bd1cb4121a6d78b2019e37e
DukeBode <Duke123@aliyun.com>
2019-12-16 14:56:31 +0800

    add

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const nunjucks = require('nunjucks')
 const sha1 = require('sha1');
-nunjucks.configure('views', { autoescape: true });
 
 /**
  * 检查签名
@@ -14,6 +13,49 @@ let checkSignature = (query, token) => {
 };
 
 /**
+ * 消息模板
+ */
+let template = nunjucks.compile(
+    '<xml>' +
+    '<ToUserName><![CDATA[{{ toUserName }}]]></ToUserName>' +
+    '<FromUserName><![CDATA[{{ fromUserName }}]]></FromUserName>' +
+    '<CreateTime>{{ createTime }}</CreateTime>' +
+    '<MsgType><![CDATA[{{ msgType }}]]></MsgType>' +
+    '{% if msgType === "text" %}' +
+    '<Content><![CDATA[{{ message.content }}]]></Content>' +
+    '{% elif msgType === "image" %}' +
+    '<Image>' +
+    '<MediaUrl><![CDATA[{{ message.mediaurl }}]]></MediaUrl>' +
+    '</Image>' +
+    '{% elif msgType === "voice" %}' +
+    '<Voice>' +
+    '<item>' +
+    '<MediaUrl><![CDATA[{{ message.mediaurl }}]]></MediaUrl>' +
+    '<Length><![CDATA[ 10]]></Length>' +
+    '</item>' +
+    '</Voice>' +
+    '{% elif msgType === "location" %}' +
+    '<Address><![CDATA[{{ message.address }}]]></Address>' +
+    '<Coordinates><![CDATA[{{ message.coordinates }}]]></Coordinates>' +
+    '{% elif msgType === "link" %}' +
+    '<Content><![CDATA[{{ message.url }}]]></Content>' +
+    '{% elif msgType === "new" %}' +
+    '<ArticleCount>{{ message.count }}</ArticleCount>' +
+    '<Articles>' +
+    '{% for article in message.articles %}' +
+    '<item>' +
+    '<Title><![CDATA[{{ article.title }}]]></Title> ' +
+    '<Description><![CDATA[{{ article.description }}]]></Description>' +
+    '<PicUrl><![CDATA[{{ article.picurl }}]]></PicUrl>' +
+    '<Url><![CDATA[{{ article.url }}]]></Url>' +
+    '</item>' +
+    '{% endfor %}' +
+    '</Articles>' +
+    '{% endif %}' +
+    '</xml>'
+    )
+
+/**
  * 内容回复模板封装
  * @param {String} msgType 
  * @param {*} fromUserName 
@@ -21,15 +63,18 @@ let checkSignature = (query, token) => {
  * @param {Object} message 
  */
 let reply = (msgType, fromUserName, toUserName, message) => {
-    return nunjucks.render('message.njk', {
+    return template.render({
         'toUserName': toUserName,
         'fromUserName': fromUserName,
-        'createTime': new Date().getTime(),
+        'createTime': Math.floor(new Date().getTime()/1000),
         'msgType': msgType,
         'message':message
     })
 };
 
+let middleware={}
 
+middleware.checkSignature=checkSignature;
+middleware.reply=reply;
 
-module.exports = checkSignature;
+module.exports = middleware;
