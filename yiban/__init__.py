32ceaa118f50309b44f5dd0b15ed4cda6c11589f
DukeBode <Duke123@aliyun.com>
2020-04-25 09:25:25 +0800

    7.0

--- yiban/__init__.py
+++ yiban/__init__.py
@@ -1,2 +1,10 @@
+'''
+@Author: your name
+@Date: 2020-04-25 09:19:10
+@LastEditTime: 2020-04-25 09:25:06
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \Yiban\yiban\__init__.py
+'''
 
-__version__ = "0.6.3.3"
+__version__ = "0.7.0.0"
